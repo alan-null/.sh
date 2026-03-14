f5f0600a098e6525471dc2b260ada4d7a02a7751
Alan Płócieniak <alan-null@outlook.com>
2026-03-14 12:36:39 +0100

    wip

--- includes/post_install.sh
+++ includes/post_install.sh
@@ -20,9 +20,11 @@ multiselect choices \
         "Terminal aliases (cls, ..)" \
     "Packages:" \
         "btop (modern top replacement)" \
+        "yazi (terminal file manager)" \
 
 echo
 
 [[ "${choices["Git aliases (gpl, gph, gc, gl...)"]}" == "true" ]] && install_git_aliases "$INSTALL_DIR" "$ZSHRC_CONF"
 [[ "${choices["Terminal aliases (cls, ..)"]}" == "true" ]] && install_terminal_aliases "$INSTALL_DIR" "$ZSHRC_CONF"
 [[ "${choices["btop (modern top replacement)"]}" == "true" ]] && sudo apt install -y btop && echo "→ btop installed"
+[[ "${choices["yazi (terminal file manager)"]}" == "true" ]] && bash "$INSTALL_DIR/includes/install_yazi.sh"
